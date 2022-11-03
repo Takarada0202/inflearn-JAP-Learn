73f679376335fcfa59e1bb087623905941fa41a3
Takarada0202 <joun0925lim@gmail.com>
2022-11-04 02:20:05 +0900

    JPA와 DB설정,동작확인

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -8,11 +8,6 @@ public class JpashopApplication {
 
 	public static void main(String[] args) {
 
-		Hello hEllo = new Hello();
-		hEllo.setData("hello");
-		String data = hEllo.getData();
-		System.out.println("data="+data);
-
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
---
+++ jpashop/src/main/java/jpabook/jpashop/Member.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String username;
+
+
+}
